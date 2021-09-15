4e5f876886c9972cdfa2e0e16fcf7d0640c6d759
MarianaRTeixeira <marianamrochateixeira@gmail.com>
2021-09-15 14:13:26 +0100

    Updates

---
+++ precache-manifest.022acf020eade288d9997859e4984caf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8510dcebcb449fbe195f",
+    "url": "/deploy_test/static/css/main.158865a3.chunk.css"
+  },
+  {
+    "revision": "8510dcebcb449fbe195f",
+    "url": "/deploy_test/static/js/main.8510dceb.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/deploy_test/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "450c4ba6e1d0b7198ce5",
+    "url": "/deploy_test/static/js/runtime~main.450c4ba6.js"
+  },
+  {
+    "revision": "bd9b7fd3544c5e4aac4574a66a1670a4",
+    "url": "/deploy_test/index.html"
+  }
+];
