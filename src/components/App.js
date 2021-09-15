4fbe17a3d4edd136d2ffe0cfdbfa8c7609b9667d
MarianaRTeixeira <marianamrochateixeira@gmail.com>
2021-09-15 14:18:40 +0100

    pages

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ import NotFound from './NotFound';
 import Featured from './Featured';
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/deploy_test">
     <div className="container">
       <Header />
       
